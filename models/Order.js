00ce1bb14fd0a9e85890a1d225927178fecb409b
Inun572 <helmi.ainun@gmail.com>
2023-12-11 12:10:07 +0700

    fix display order details with table3

--- models/Order.js
+++ models/Order.js
@@ -1,4 +1,6 @@
 import { faker } from '@faker-js/faker';
+import Table from 'cli-table3';
+import { moneyFormatter } from '../libs/formatter.js';
 
 export default class Order {
   constructor(cart) {
@@ -9,15 +11,44 @@ export default class Order {
   }
 
   createOrder(cart) {
-    this.items.push(cart.items);
+    cart.items.forEach((item) => {
+      this.items.push(item);
+    });
     this.totalItems = cart.totalItems;
     this.totalAmount = cart.totalPrice;
   }
 
   displayOrderDetails() {
+    const table = new Table({
+      head: [
+        { hAlign: 'center', content: 'Name' },
+        { hAlign: 'center', content: 'Price' },
+        { hAlign: 'center', content: 'Qty' },
+        { hAlign: 'center', content: 'Total' },
+      ],
+      colWidths: [20, 20, 5, 20],
+    });
+
+    this.items.forEach((item) => {
+      table.push([
+        { hAlign: 'center', content: item.name },
+        { hAlign: 'center', content: moneyFormatter(item.price) },
+        { hAlign: 'center', content: item.qty },
+        { hAlign: 'right', content: moneyFormatter(item.total) },
+      ]);
+    });
+
+    table.push([
+      { colSpan: 3, content: 'Total', hAlign: 'right' },
+      {
+        content: moneyFormatter(this.totalAmount),
+        hAlign: 'right',
+      },
+    ]);
+
     console.log(`Order ID: ${this.id}`);
     console.log(`Total Items: ${this.totalItems}`);
-    console.log(`Items: ${this.items}`);
-    console.log(`Total Amount: ${this.totalAmount}`);
+    console.log(`Details order:\n-----------------`);
+    console.log(table.toString());
   }
 }
